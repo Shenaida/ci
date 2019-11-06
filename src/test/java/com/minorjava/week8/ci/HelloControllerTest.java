56dd50132b5c1251cc5d651e765b149d12b13bb0
Shenaida <shenaidaoemar@hotmail.com>
2019-11-06 20:13:24 +0100

    fixed failing test in helloControllerTest

--- src/test/java/com/minorjava/week8/ci/HelloControllerTest.java
+++ src/test/java/com/minorjava/week8/ci/HelloControllerTest.java
@@ -24,9 +24,9 @@ public class HelloControllerTest {
 
     @Test
     public void getHello() throws Exception {
-        mvc.perform(MockMvcRequestBuilders.get("/").accept(MediaType.APPLICATION_JSON))
+        mvc.perform(MockMvcRequestBuilders.get("/hello").accept(MediaType.APPLICATION_JSON))
                 .andExpect(status().isOk())
-                .andExpect(content().string(equalTo("{\"greeting\":\n" +
-                        "\"Hello, world!\"}")));
+                .andExpect(content().string(equalTo("[{\"greeting\":" +
+                        "\"Hello World!\"}]")));
     }
 }
