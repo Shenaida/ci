6a529df19241a1923dc2dce7d8ea17741d598d6d
Shenaida <shenaidaoemar@hotmail.com>
2019-11-06 10:28:48 +0100

    update helloController with world to test CI

--- src/main/java/com/minorjava/week8/ci/HelloController.java
+++ src/main/java/com/minorjava/week8/ci/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
     @RequestMapping("/")
     public String index() {
         return "{\"greeting\":\n" +
-                "\"Hello, everybody!\"}";
+                "\"Hello, world!\"}";
     }
 
 }
